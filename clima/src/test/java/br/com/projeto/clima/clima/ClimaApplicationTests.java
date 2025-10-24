04ac22bb05183d3d124d86413dc5d38d2ef70b19
oRonold <or040918@gmail.com>
2025-10-24 15:36:54 -0300

    initial commit

---
+++ clima/src/test/java/br/com/projeto/clima/clima/ClimaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.clima.clima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ historico/src/main/java/br/com/projeto/clima/historico/HistoricoApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.clima.historico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricoApplication.class, args);
+	}
+
+}
