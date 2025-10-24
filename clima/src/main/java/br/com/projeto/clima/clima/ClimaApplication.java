209ddc53726c910305543ba2fcc225b6749ddf49
oRonold <or040918@gmail.com>
2025-10-24 17:20:06 -0300

    adicionados discovery client e rabbitmq

--- clima/src/main/java/br/com/projeto/clima/clima/ClimaApplication.java
+++ clima/src/main/java/br/com/projeto/clima/clima/ClimaApplication.java
@@ -1,9 +1,14 @@
 package br.com.projeto.clima.clima;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableRabbit
 public class ClimaApplication {
 
 	public static void main(String[] args) {
