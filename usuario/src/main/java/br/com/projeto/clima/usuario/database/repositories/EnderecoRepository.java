4ea9886c4667326f13a73252a14e629f8729e9e5
oRonold <or040918@gmail.com>
2025-10-25 00:04:40 -0300

    refatorando repositories

--- usuario/src/main/java/br/com/projeto/clima/usuario/database/repositories/EnderecoRepository.java
+++ usuario/src/main/java/br/com/projeto/clima/usuario/database/repositories/EnderecoRepository.java
@@ -1,4 +1,4 @@
-package br.com.projeto.clima.usuario.database.infrastructure;
+package br.com.projeto.clima.usuario.database.repositories;
 
 import br.com.projeto.clima.usuario.database.domain.endereco.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
