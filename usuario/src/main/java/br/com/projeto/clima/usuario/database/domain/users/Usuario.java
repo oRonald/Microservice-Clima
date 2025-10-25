3396b31d8d54f247fc747122b05f9399d0ea2a7c
oRonold <or040918@gmail.com>
2025-10-25 15:59:30 -0300

    definindo atributo email como unique true

--- usuario/src/main/java/br/com/projeto/clima/usuario/database/domain/users/Usuario.java
+++ usuario/src/main/java/br/com/projeto/clima/usuario/database/domain/users/Usuario.java
@@ -18,7 +18,7 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
     @Column(nullable = false)
     private String name;
