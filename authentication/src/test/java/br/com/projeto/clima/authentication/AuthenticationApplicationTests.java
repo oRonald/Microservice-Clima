b7a4c9666a56cf83c5152651cb40452172933693
oRonold <or040918@gmail.com>
2025-10-26 23:10:41 -0300

    adicionando microsserviço de authentication

---
+++ authentication/src/test/java/br/com/projeto/clima/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.clima.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
