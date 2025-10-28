b379ea3710f9458d21ce2bfc8459da71d08a2d1b
oRonold <or040918@gmail.com>
2025-10-27 22:54:01 -0300

    removendo validation with authentication service

--- usuario/src/main/java/br/com/projeto/clima/usuario/handler/exceptions/ValidationFailedException.java
+++
@@ -1,8 +0,0 @@
-package br.com.projeto.clima.usuario.handler.exceptions;
-
-public class ValidationFailedException extends RuntimeException {
-
-    public ValidationFailedException(String loginValidationFailed) {
-
-    }
-}
--- usuario/src/main/java/br/com/projeto/clima/usuario/service/usuario/IUsuarioValidateService.java
+++
@@ -1,9 +0,0 @@
-package br.com.projeto.clima.usuario.service.usuario;
-
-import br.com.projeto.clima.usuario.dto.UserLoginEmail;
-import br.com.projeto.clima.usuario.dto.UserLoginRequest;
-
-public interface IUsuarioValidateService {
-
-    UserLoginEmail usuarioValidate(UserLoginRequest request);
-}
