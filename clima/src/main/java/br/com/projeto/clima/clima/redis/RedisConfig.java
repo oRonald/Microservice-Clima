3cd651268ad7c0e450afba7054ac6144e088cd3a
oRonold <or040918@gmail.com>
2025-10-29 16:46:28 -0300

    definindo novo metodo de serialização no Redis

--- clima/src/main/java/br/com/projeto/clima/clima/redis/RedisConfig.java
+++ clima/src/main/java/br/com/projeto/clima/clima/redis/RedisConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -25,6 +28,16 @@ public class RedisConfig {
     public RedisTemplate<String, WeatherResponse> redisTemplate(){
         RedisTemplate<String, WeatherResponse> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
+
+        GenericJackson2JsonRedisSerializer valueSerializer = new GenericJackson2JsonRedisSerializer();
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(valueSerializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(valueSerializer);
+
+        template.afterPropertiesSet();
+
         return template;
     }
 }
