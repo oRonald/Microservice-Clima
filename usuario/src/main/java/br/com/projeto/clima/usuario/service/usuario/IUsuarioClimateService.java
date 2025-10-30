9acef4344a0b95ea617567768bc81e86075e57d1
oRonold <or040918@gmail.com>
2025-10-29 21:54:03 -0300

    interface para retornar response para o usuario

--- usuario/src/main/java/br/com/projeto/clima/usuario/service/usuario/IUsuarioClimateService.java
+++ usuario/src/main/java/br/com/projeto/clima/usuario/service/usuario/IUsuarioClimateService.java
@@ -1,9 +1,13 @@
 package br.com.projeto.clima.usuario.service.usuario;
 
-import org.springframework.http.ResponseEntity;
+import br.com.projeto.clima.usuario.dto.WeatherResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 public interface IUsuarioClimateService {
 
-    ResponseEntity<?> getPerfil(@RequestHeader("Authorization") String authorization);
+    String sendUsersCity(@RequestHeader("Authorization") String authorization) throws JsonProcessingException;
+    WeatherResponse getCachedResponse(String requestId);
+
+    void putResponse(String requestId, WeatherResponse response);
 }
