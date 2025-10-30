6b72a7667ff214ced1ab1a26ae9be6e653a689e3
oRonold <or040918@gmail.com>
2025-10-29 21:56:51 -0300

    implementando service que controla fluxo entre usuario e clima

--- usuario/src/main/java/br/com/projeto/clima/usuario/service/usuario/implementation/UsuarioClimateService.java
+++ usuario/src/main/java/br/com/projeto/clima/usuario/service/usuario/implementation/UsuarioClimateService.java
@@ -2,21 +2,51 @@ package br.com.projeto.clima.usuario.service.usuario.implementation;
 
 import br.com.projeto.clima.usuario.database.domain.users.Usuario;
 import br.com.projeto.clima.usuario.database.repositories.UsuarioRepository;
+import br.com.projeto.clima.usuario.dto.ClientDataPublish;
+import br.com.projeto.clima.usuario.dto.WeatherResponse;
+import br.com.projeto.clima.usuario.handler.exceptions.EmailDoesNotExistsException;
+import br.com.projeto.clima.usuario.service.mqueue.UsersWeatherPublisher;
 import br.com.projeto.clima.usuario.service.usuario.IUsuarioClimateService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
-@RequiredArgsConstructor
 public class UsuarioClimateService implements IUsuarioClimateService {
 
+    private final UsersWeatherPublisher publisher;
     private final UsuarioRepository repository;
+    private final Map<String, WeatherResponse> responseCache = new ConcurrentHashMap<>();
 
-    @GetMapping
-    public ResponseEntity<?> getPerfil(String authorization) {
-        String token = authorization.replace("Bearer ", "");
-        return ResponseEntity.ok(token);
+    public UsuarioClimateService(UsersWeatherPublisher publisher, UsuarioRepository repository) {
+        this.publisher = publisher;
+        this.repository = repository;
+    }
+
+    @Override
+    public String sendUsersCity(String authorization) {
+        Usuario usuario = repository.findByEmail(authorization).orElseThrow(EmailDoesNotExistsException::new);
+
+        String requestId = UUID.randomUUID().toString();
+
+        publisher.requestWeather(ClientDataPublish.builder()
+                .cityName(usuario.getAddress().getCity())
+                .requestId(requestId)
+                .build());
+
+        return requestId;
+    }
+
+    @Override
+    public void putResponse(String requestId, WeatherResponse response) {
+        responseCache.put(requestId, response);
+    }
+
+    @Override
+    public WeatherResponse getCachedResponse(String requestId) {
+        return responseCache.get(requestId);
     }
 }
