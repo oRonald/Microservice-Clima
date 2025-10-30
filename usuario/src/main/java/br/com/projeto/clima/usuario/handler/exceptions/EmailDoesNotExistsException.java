ee5df23127276eed1a75a1623e73a58904c6adc4
oRonold <or040918@gmail.com>
2025-10-29 21:51:54 -0300

    adicionando EmailDoesNotExistsException

---
+++ usuario/src/main/java/br/com/projeto/clima/usuario/handler/exceptions/EmailDoesNotExistsException.java
@@ -0,0 +1,7 @@
+package br.com.projeto.clima.usuario.handler.exceptions;
+
+public class EmailDoesNotExistsException extends RuntimeException {
+
+    public EmailDoesNotExistsException() {
+    }
+}
