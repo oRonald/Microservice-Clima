c82b5a4b91c9b6aaa6c5e99dd8d3d923b5e810b6
oRonold <or040918@gmail.com>
2025-10-29 21:53:20 -0300

    API externa exception

---
+++ clima/src/main/java/br/com/projeto/clima/clima/handler/exception/WeatherStackException.java
@@ -0,0 +1,7 @@
+package br.com.projeto.clima.clima.handler.exception;
+
+public class WeatherStackException extends Exception{
+
+    public WeatherStackException(String message, int status, String errorBody) {
+    }
+}
