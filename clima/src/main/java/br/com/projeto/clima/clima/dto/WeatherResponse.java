d2b8a0804e6974df6e184f7219b1207c63eaff8f
oRonold <or040918@gmail.com>
2025-10-29 21:54:39 -0300

    DTO para receber os dados da API weatherstack

---
+++ clima/src/main/java/br/com/projeto/clima/clima/dto/WeatherResponse.java
@@ -0,0 +1,65 @@
+package br.com.projeto.clima.clima.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WeatherResponse implements Serializable {
+
+    private Location location;
+    private CurrentWeather current;
+
+    private String requestId;
+
+    @Data
+    public static class Location implements Serializable {
+
+        private Long id;
+
+        @JsonProperty("name")
+        private String cityName;
+
+        private String country;
+
+        @JsonProperty("timezone_id")
+        private String timeZoneId;
+
+        @JsonProperty("localtime")
+        private String localTime;
+
+        private LocalDateTime requestTime;
+    }
+
+    @Data
+    public static class CurrentWeather implements Serializable {
+
+        private Long id;
+
+        private Integer temperature;
+
+        @JsonProperty("weather_descriptions")
+        private List<String> weatherDescriptions;
+
+        @JsonProperty("wind_speed")
+        private Integer windSpeed;
+
+        @JsonProperty("precip")
+        private Integer precipitation;
+
+        @JsonProperty("feelslike")
+        private Integer feelsLike;
+
+        @JsonProperty("is_day")
+        private String isDayRaw;
+    }
+}
