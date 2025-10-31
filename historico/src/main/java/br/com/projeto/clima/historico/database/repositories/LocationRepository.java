1ed7b023b61114210be699d0b50336c1f29a75d3
oRonold <or040918@gmail.com>
2025-10-30 22:26:00 -0300

    removendo historico microservice

--- historico/src/main/java/br/com/projeto/clima/historico/database/repositories/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.projeto.clima.historico.database.repositories;
-
-import br.com.projeto.clima.historico.database.domain.location.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
